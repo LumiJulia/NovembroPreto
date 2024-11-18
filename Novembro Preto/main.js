0decbded99f4539e22b804c837ae453a37719366
Julia Lumi Suzuki <133894726+LumiJulia@users.noreply.github.com>
2024-11-18 07:42:15 -0300

    Delete Novembro Preto directory

--- Novembro Preto/main.js
+++
@@ -1,7 +0,0 @@
-//Modo noturno
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    const currentMode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
-    localStorage.setItem('theme', currentMode);
-}
-
